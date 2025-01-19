044409512252326d01fce00ce02de1fe29ebada4
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2025-01-19 11:44:00 +0100

    Created news model

--- Practice(Algorithms)/Core/Model/NewsModel.swift
+++ Practice(Algorithms)/Core/Model/NewsModel.swift
@@ -7,7 +7,25 @@
 
 import UIKit
 
-
-struct NewsModel: Identifiable, Codable {
-    let id: Int
+struct NewsResponse: Decodable {
+    let status: String
+    let totalResults: Int
+    let articles: [NewsModel]
 }
+
+struct NewsModel: Decodable {
+    let source: NewsSource
+    let author: String
+    let title: String
+    let description: String
+    let url: String
+    let urlToImage: String
+    let publishedAt: String
+    let content: String
+}
+
+struct NewsSource: Identifiable, Decodable {
+    let id: String
+    let name: String
+}
+ 
