9918b88cb1f2ba618e59dd6f0ad6cfcd6c7c05e8
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2025-01-19 09:22:02 +0100

    Initial Commit

---
+++ Practice(Algorithms)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice(Algorithms)
+//
+//  Created by Roman Samborskyi on 19.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Practice(Algorithms)/Practice_Algorithms_App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Algorithms_App.swift
+//  Practice(Algorithms)
+//
+//  Created by Roman Samborskyi on 19.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Algorithms_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
